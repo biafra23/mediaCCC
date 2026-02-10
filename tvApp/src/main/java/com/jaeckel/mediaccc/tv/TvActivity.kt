24aa186fbf1452a7855d8c2677ba8b6ee662f372
Dirk Jäckel <dirk@jaeckel.com>
2026-02-10 14:58:40 +0100

    Separate Android TV UI from Android phone & tablet UI.

--- tvApp/src/main/java/com/jaeckel/mediaccc/tv/TvActivity.kt
+++ tvApp/src/main/java/com/jaeckel/mediaccc/tv/TvActivity.kt
@@ -1,23 +1,33 @@
 package com.jaeckel.mediaccc.tv
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jaeckel.mediaccc.App
 
 class TvActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            TvEntryScreen()
         }
+    }
+}
+
+@Composable
+fun TvEntryScreen() {
+    MaterialTheme {
+        Text("Running on TV",  color = androidx.compose.ui.graphics.Color.White)
     }
 }
 
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    TvEntryScreen()
 }
