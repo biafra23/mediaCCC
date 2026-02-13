63c73f20374dcc65d33d2ced799a129e991da1a5
Dirk Jäckel <dirk@jaeckel.com>
2026-02-13 09:52:48 +0100

    Use Greeting in tvApp

--- tvApp/src/main/java/com/jaeckel/mediaccc/tv/TvActivity.kt
+++ tvApp/src/main/java/com/jaeckel/mediaccc/tv/TvActivity.kt
@@ -7,8 +7,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jaeckel.mediaccc.Greeting
 
 class TvActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,8 @@ class TvActivity : ComponentActivity() {
 @Composable
 fun TvEntryScreen() {
     MaterialTheme {
-        Text("Running on TV",  color = androidx.compose.ui.graphics.Color.White)
+        val greeting = remember { Greeting().greet() }
+        Text("Compose: $greeting",  color = androidx.compose.ui.graphics.Color.White)
     }
 }
 
