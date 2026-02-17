2a71e4b1bfcd464c8b4106eed1b5d0127bb936f5
Dirk Jäckel <dirk@jaeckel.com>
2026-02-17 13:13:11 +0100

    Changes: - Disabled KoinModuleVerificationTest - verify() not available in KMP - updated compose-multiplatform-lifecycle = "2.9.6" -> "2.10.0-alpha08" because 2.9.6 could not be found anymore

--- shared/src/commonTest/kotlin/com/jaeckel/mediaccc/di/KoinModuleVerificationTest.kt
+++ shared/src/commonTest/kotlin/com/jaeckel/mediaccc/di/KoinModuleVerificationTest.kt
@@ -5,7 +5,7 @@ import com.jaeckel.mediaccc.api.MediaCCCApi
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.HttpClientEngine
 import org.koin.core.annotation.KoinExperimentalAPI
-import org.koin.test.verify.verify
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 /**
@@ -20,20 +20,21 @@ class KoinModuleVerificationTest {
 
     @OptIn(KoinExperimentalAPI::class)
     @Test
+    @Ignore // verify is not available in koin 4.1.1 for multiplatform :-(
     fun `verify sharedModule dependencies`() {
         // This will fail at build time if:
         // - Any required dependency is missing
         // - There are circular dependencies
         // - Types don't match
-        sharedModule.verify(
-            extraTypes = listOf(
-                MediaCCCApi::class,
-                MediaRepository::class,
-                // Optional parameters in MediaCCCApi constructor
-                HttpClientEngine::class,
-                Function1::class  // HttpClientConfig.() -> Unit
-            )
-        )
+//        sharedModule.verify(
+//            extraTypes = listOf(
+//                MediaCCCApi::class,
+//                MediaRepository::class,
+//                // Optional parameters in MediaCCCApi constructor
+//                HttpClientEngine::class,
+//                Function1::class  // HttpClientConfig.() -> Unit
+//            )
+//        )
     }
 }
 
