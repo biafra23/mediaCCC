6a28a44e396f5ba5fbcefdf3028e9663da6824e5
Dirk Jäckel <dirk@jaeckel.com>
2026-02-21 09:42:47 +0100

    Fix warning about Instant deprecation

--- api/src/commonMain/kotlin/com/jaeckel/mediaccc/api/model/Event.kt
+++ api/src/commonMain/kotlin/com/jaeckel/mediaccc/api/model/Event.kt
@@ -1,6 +1,6 @@
 package com.jaeckel.mediaccc.api.model
 
-import kotlinx.datetime.Instant
+import kotlin.time.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
